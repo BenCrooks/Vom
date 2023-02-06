91729fdbafdb1b5dd442d7423d85e8485a744873
Tobias <tobiassherco125@gmail.com>
2023-02-06 09:29:53 +0200

    Ui elements added
    Menu layout changed
    Settings scene added
    Loading screen added with delay

--- Assets/Scripts/Menus/LevelLoad.cs
+++ Assets/Scripts/Menus/LevelLoad.cs
@@ -2,11 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class LevelLoad : MonoBehaviour
 {
+
+    public GameObject loadScreen;
     public void Play()
     {
+        loadScreen.SetActive(true);
         SceneManager.LoadScene("Gameplay");
 
     }
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+
+    }
+    public void Return()
+    {
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+
 }
